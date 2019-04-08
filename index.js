91413f4f4f6357a5aa88a1176b7002d397bdf38f
janjarfalk <jan@jarfalk.se>
2019-04-08 22:38:06 +0200

    adds sound

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const SIZE = 270;
 const CENTER = SIZE / 2;
 
+let mouse = {
+    x: 0.5,
+    y: 0.4
+};
+
 // A gray-scale height-map.
 const COLORS = [
     [255, 255, 255],
@@ -52,12 +57,12 @@ function rain() {
             const isRain = y < 2;
 
             // Generate a flooded street in the bottom of the image.
-            const isStreet = y > Math.round(CENTER*1.53) && y < Math.round(CENTER*1.57);
+            const isStreet = y > Math.round(CENTER * 1.53) && y < Math.round(CENTER * 1.57);
 
             // Mask out the silhouette of an umbrella in the middle of the image.
             const isUmbrella = Math.random() > 0.8
                 && isInsideCircle(CENTER, CENTER, x, y, 32)
-                && !isInsideCircle(CENTER, CENTER - 10, x, y, 40);
+                && !isInsideCircle(CENTER, CENTER - 10, x - 1, y, 40);
 
             if (isRain || isStreet || isUmbrella) {
                 levelBufferViewInt8[index] = 0;
@@ -80,24 +85,14 @@ function moveDroplet(src) {
     // Generate a 0, 1 or 2.
     const rand = Math.round(Math.random() * 2.0);
 
-    // Wind alternatives...
-    // A little bit of wind.
-    // const horizontalMovement = src - rand + 1;
-
-    // No wind.
-    // const horizontalMovement = src;
-
-    // A lot of wind.
-    // const horizontalMovement = src - rand - 2;
-
-    // A little bit of both.
+    // Add horizontal movement to create a wind effect.
     const horizontalMovement = Math.random() > 0.8
-        ? src - rand + 1
+        ? src - rand + Math.floor((mouse.x * 8 - 3))
         : src;
 
     // Every 1 in 5 the rain will move up-wards and create the
     // street splash effect.
-    const verticalMovement = Math.floor(Math.random() * 4.5) * (Math.random() > 0.2
+    const verticalMovement = Math.floor(Math.random() * (4 + Math.floor((mouse.y * 4)))) * (Math.random() > 0.2
         ? 1
         : -1);
 
@@ -117,8 +112,83 @@ function render() {
     rain();
 
     // Go again!
-    requestAnimationFrame(() => render());
+    // setTimeout(function () {
+        requestAnimationFrame(() => render());
+    // }, 1000);
 }
 
 // Go!
 render();
+
+
+var Noise = (function () {
+
+    const audioContext = new (window.AudioContext || window.webkitAudioContext);
+
+    function createNoise(track) {
+        var lastOut = 0.0;
+        const bufferSize = 2 * audioContext.sampleRate;
+        const noiseBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
+        const output = noiseBuffer.getChannelData(0);
+
+        for (let i = 0; i < bufferSize; i++) {
+            var white = Math.random() * 2 - 1;
+            output[i] = (lastOut + (0.02 * white)) / 1.02;
+            lastOut = output[i];
+            output[i] *= 3.5;
+        }
+
+        track.audioSource.buffer = noiseBuffer;
+    }
+
+    function stopNoise(track) {
+        if (track.audioSource) {
+            track.audioSource.stop();
+        }
+    }
+
+
+    function buildTrack(track) {
+        track.audioSource = audioContext.createBufferSource();
+        track.gainNode = audioContext.createGain();
+        track.audioSource.connect(track.gainNode);
+        track.gainNode.connect(audioContext.destination);
+    }
+
+    function setGain(track) {
+        track.volume = 0.1;
+        track.gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+        track.gainNode.gain.linearRampToValueAtTime(track.volume / 4, audioContext.currentTime / 2);
+        track.gainNode.gain.linearRampToValueAtTime(track.volume, audioContext.currentTime);
+
+    }
+
+    function playNoise() {
+
+        const track = {}
+
+        stopNoise(track);
+        buildTrack(track);
+        createNoise(track);
+        setGain(track);
+        track.audioSource.loop = true;
+        track.audioSource.start();
+    }
+
+    // Expose functions:
+    return {
+        play: playNoise,
+        stop: stopNoise
+    }
+
+}());
+
+
+let handleMousemove = (event) => {
+    mouse.x = Math.floor(event.x / window.innerWidth * 100) / 100;
+    mouse.y = Math.floor(event.y / window.innerHeight * 100) / 100;
+};
+
+document.addEventListener('mousemove', handleMousemove);
+
+canvas.addEventListener('click', () => Noise.play());
